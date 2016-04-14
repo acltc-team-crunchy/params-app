adff33979debaa9e8027e207b99786aa2b413f81
Mark Richardson <markeban@yahoo.com>
2016-04-13 21:20:25 -0700

    finished with url segment parameters

---
+++ app/controllers/params_controller.rb
@@ -0,0 +1,29 @@
+class ParamsController < ApplicationController
+
+  def name
+    @name = params[:first_name].upcase
+    p @name[0]
+    if @name[0] == "A"
+      @message =  "Hey, your name starts with the first letter of the alphabet!"
+    end
+  end
+
+
+  def number_game
+    secret_number = 67
+    number = params[:number].to_i
+    if number > secret_number
+      @message = "Too Big"
+    elsif number < secret_number
+      @message = "Too Small"
+    else
+      @message = "You win!"
+    end
+  end
+
+  def url_segment
+    @student_1 = params[:student_1]
+    @student_2 = params[:student_2]
+
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,41 @@
+Rails.application.routes.draw do
+
+  get 'name', to: 'params#name'
+  get 'number_game', to: 'params#number_game'
+
+  get 'url_segment/:student_1/:student_2', to: 'params#url_segment'
+
+
+
+
+
+
+
+  # Example resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
+
+  # Example resource route with more complex sub-resources:
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', on: :collection
+  #     end
+  #   end
+
+  # Example resource route with concerns:
+  #   concern :toggleable do
+  #     post 'toggle'
+  #   end
+  #   resources :posts, concerns: :toggleable
+  #   resources :photos, concerns: :toggleable
+
+  # Example resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
+end
